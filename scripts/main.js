338f175704919d759c1ddcfe7299b5f2fdebab87
hrlnsnchz <hrlnsnchz@gmail.com>
2021-02-16 10:43:38 -0600

    The form is now rendered as a component on the DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { getEntries } from "./JournalDataProvider.js";
 import { EntryListComponent } from "./JournalEntryList.js";
+import { JournalFormComponent } from "./JournalForm.js";
 
-
+JournalFormComponent()
 getEntries()
 EntryListComponent()
